62e9ac53ebfc26af4c0c635d1a1fafef1a043656
Tadeo Durazo <tadeo_97@outlook.com>
2024-02-18 21:02:03 -0600

    feat: light and dark mode note colors

---
+++ lib/ui/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final darkModeColors = [
+    Colors.grey.shade900,
+    Colors.lightBlue,
+    Colors.grey,
+    Colors.green,
+    Colors.blueAccent,
+    Colors.pinkAccent,
+    Colors.tealAccent,
+  ];
+
+  static final lightModeColors = [
+    Colors.white,
+    Colors.lightBlue,
+    Colors.grey,
+    Colors.greenAccent,
+    Colors.blueAccent,
+    Colors.pinkAccent,
+    Colors.tealAccent,
+  ];
+}
