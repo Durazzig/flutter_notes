910a46d8884fb7b89bc46901b2fc91f3ae6b72de
Tadeo Durazo <tadeo_97@outlook.com>
2024-02-18 21:54:13 -0600

    feat: store selected theme in local storage

--- lib/features/shared/theme_provider.dart
+++ lib/features/shared/theme_provider.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notes/features/shared/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeData _themeData = lightMode;
 
   ThemeData get themeData => _themeData;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
 
   set themeData(ThemeData themeData) {
     _themeData = themeData;
@@ -13,5 +18,19 @@ class ThemeProvider extends ChangeNotifier {
 
   void changeTheme() {
     themeData = _themeData == lightMode ? darkMode : lightMode;
+    _saveTheme(_themeData == darkMode ? 'dark' : 'light');
+  }
+
+  _saveTheme(String theme) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isDarkTheme = theme == 'dark';
+    prefs.setBool('isDarkTheme', isDarkTheme);
+  }
+
+  _loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isDarkTheme = prefs.getBool('isDarkTheme') ?? false;
+    _themeData = isDarkTheme ? darkMode : lightMode;
+    notifyListeners();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,15 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: const MyApp(),
-    ),
-  );
+  final themeProvider = ThemeProvider();
+  Future.delayed(const Duration(milliseconds: 100), () {
+    runApp(
+      ChangeNotifierProvider(
+        create: (context) => themeProvider,
+        child: const MyApp(),
+      ),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
