656fea9e2df7e277c54b88b050ec7aeb386c0a95
Tadeo Durazo <tadeo_97@outlook.com>
2024-02-18 21:02:50 -0600

    feat: new app theme secondary color

--- lib/features/shared/theme.dart
+++ lib/features/shared/theme.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
-  colorScheme: const ColorScheme.light(
+  colorScheme: ColorScheme.light(
     background: Colors.white,
     primary: Colors.purple,
+    secondary: Colors.grey.shade900,
   ),
   textTheme: const TextTheme(
     titleMedium: TextStyle(
@@ -19,6 +20,7 @@ ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     background: Colors.grey.shade900,
     primary: Colors.white,
+    secondary: Colors.white,
   ),
   textTheme: const TextTheme(
     titleMedium: TextStyle(
